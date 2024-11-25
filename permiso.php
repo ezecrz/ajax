a1fec1f8b2f2ae81e1dcf4aba8a114acdaaedd16
ezecrz <otro.ezequielcruz@gmail.com>
2024-11-24 19:14:41 -0600

    Update permiso.php

--- permiso.php
+++ permiso.php
@@ -1,28 +1,31 @@
- <?php 
+<?php 
 require_once "../modelos/Permiso.php";
 
-$categoria=new Permiso();
-
+$categoria = new Permiso();
 
 switch ($_GET["op"]) {
-	
     case 'listar':
-		$rspta=$categoria->listar();
-		$data=Array();
+        $rspta = $categoria->listar();
+        $data = Array();
 
-		while ($reg=$rspta->fetch_object()) {
-			$data[]=array(
-            
-            "0"=>$reg->nombre
-            
-              );
-		}
-		$results=array(
-             "sEcho"=>1,//info para datatables
-             "iTotalRecords"=>count($data),//enviamos el total de registros al datatable
-             "iTotalDisplayRecords"=>count($data),//enviamos el total de registros a visualizar
-             "aaData"=>$data); 
-		echo json_encode($results);
-		break;
+        while ($reg = $rspta->fetch_object()) {
+            $data[] = array(
+                "0" => $reg->nombre
+            );
+        }
+        $results = array(
+            "sEcho" => 1, // info para datatables
+            "iTotalRecords" => count($data), // enviamos el total de registros al datatable
+            "iTotalDisplayRecords" => count($data), // enviamos el total de registros a visualizar
+            "aaData" => $data
+        );
+        echo json_encode($results);
+        break;
+
+    // Agrega la cláusula default para manejar los casos no definidos
+    default:
+        // Acción predeterminada en caso de que no se pase un valor válido en $_GET["op"]
+        echo json_encode(array("error" => "Opción no válida"));
+        break;
 }
- ?>
+?>
